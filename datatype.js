1a5e5b83702778076b1bd44e1a7a383510238d30
pascalk5 <pascalrunkarhanu@gmail.com>
2025-01-27 14:24:52 +0300

    initial commit

---
+++ datatype.js
@@ -0,0 +1,43 @@
+//Declaring variable 
+const firstVar = "Pascal";
+const secondVar = true;
+const idStudent = false;
+const variable1 = undefined;
+const variable2 = null;
+
+// showing the typeof each variable
+console.log(typeof(firstVar));
+console.log(typeof(secondVar));
+console.log(typeof(idStudent));
+console.log(typeof(variable1));
+console.log(typeof(variable2));
+
+// declaring variable with no value
+const id = '';
+const studentName = '';
+const departmentName = '';
+const courseName = '';
+
+//declaring variable with value
+const name1 = "Pascal";
+const age2 = 22;
+const nationality3 = 'Congolese';
+const address4 = 'Nsambya kirombe';
+
+//declaring variables to store some details on multiple line
+const firstName = 'Pascal';
+const lastName = 'Karhanu';
+const maritalStatus = 'Single';
+const country = 'RDC';
+
+//declaring variables to store some details on single line
+const firstName1 = 'Pascal'; const lastName2 = 'Karhanu'; const maritalStatus3 = 'Single'; const country4 = 'RDC';
+
+//declaring variables to store myAge and your age then showing them in a console.log
+const myAge = 22;
+const yourAge = 24;
+
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
+
+
---
+++ variable.js
@@ -0,0 +1,5 @@
+const firstName = "Pascal";
+const name = true;
+const idStudent = false;
+const variable1 = undefined;
+const variable2 = null;
