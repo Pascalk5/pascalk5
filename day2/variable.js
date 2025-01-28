0d20f2a463797286c9543700ee6c320b13f8d448
pascalk5 <pascalrunkarhanu@gmail.com>
2025-01-28 15:24:49 +0300

    added

--- day2/variable.js
+++ day2/variable.js
@@ -1,5 +1,14 @@
-const challenge ='Coding Days of JavaScritp';
+const challenge ='Coding Days of JavaScript';
+const str = challenge.indexOf(" ");
+const resultStr = challenge.substring(str + 1);
+
+const str1 = "Days of JavaScript";
+const resultStr1 = challenge.replace(str1, "");
+
 console.log(challenge);
 console.log(challenge.length);
 console.log(challenge.toUpperCase());
 console.log(challenge.toLowerCase());
+console.log(resultStr);
+console.log(resultStr1);
+
